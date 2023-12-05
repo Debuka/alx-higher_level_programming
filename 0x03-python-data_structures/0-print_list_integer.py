38cc6dfef0fb33d8553f066c058d600fa3be8638
Debuka <bukasondean@gmail.com>
2023-12-05 12:52:01 -0800

    pythonn data structure

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """
+    Function that print all integers of a list.
+    """
+    for m in my_list:
+        print("{:d}".format(m))
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+i#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+    Function that retrieve an element from a list.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
